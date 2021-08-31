6814b792ab9e6d81b6f7fa9adc38135e00ea796c
mike7772 <michaelasnake644@gmail.com>
2021-08-31 09:33:54 -0700

    function

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+* main - Entry point
+*
+* Return: Always 0
+*/
 int main(void)
 {
 _putchar('_');
-        _putchar('p');
+_putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
@@ -15,6 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
 return (0);
 }
