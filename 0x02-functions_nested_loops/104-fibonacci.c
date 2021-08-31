7bd7c9704b471db7140ed17b84ba8bd4dea37bb3
mike7772 <michaelasnake644@gmail.com>
2021-08-31 10:35:16 -0700

    functions

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,22 +1,22 @@
-include "main.h"
+#include "main.h"
 /** 
 * main - Prints the first 98 fibonacci numbers
 * * Return: Nothing! 
-*/int main(void){	
-int i = 0;	
+*/int main(void){
+int i = 0;
 Int j=1, k=2;
-while (i < 96)	{		
-if (i == 0)			
-printf("%ld", j);		
-else if (i == 1)			
-printf(", %ld", k);		
-else {			
-k += j;	
-j = k - j;			
-printf(", %ld", k);		
-} 		
-++i;	
-} 	
-printf("\n");	
+while (i < 96){
+if (i == 0)
+printf("%ld", j);
+else if (i == 1)
+printf(", %ld", k);
+else {
+k += j;
+j = k - j;
+printf(", %ld", k);
+} 
+++i;
+} 
+printf("\n");
 return (0);
 }
