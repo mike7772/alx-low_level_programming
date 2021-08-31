e41ed398e7dd38fafa42ae4a8a2bba212ad2ec21
mike7772 <michaelasnake644@gmail.com>
2021-08-31 10:41:55 -0700

    functions

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,19 +4,19 @@
 * * Return: Nothing! 
 */int main(void){
 int i = 0;
-Int j=1, k=2;
-while (i < 96){
-if (i == 0)
-printf("%ld", j);
-else if (i == 1)
-printf(", %ld", k);
-else {
-k += j;
-j = k - j;
-printf(", %ld", k);
-} 
-++i;
-} 
-printf("\n");
+int j=1, k=2;
+while (i < 96)	{		
+if (i == 0)			
+_putchar(j);		
+else if (i == 1)			
+_putchar(k);		
+else {			
+k += j;	
+j = k - j;			
+_putchar(k);		
+} 		
+++i;	
+} 	
+_putchar(' ');
 return (0);
 }
