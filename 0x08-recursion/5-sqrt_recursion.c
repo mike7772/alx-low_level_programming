4b6519def0b5596e061f74f88f1004221741ed7a
mike7772 <michaelasnake644@gmail.com>
2021-09-15 09:03:10 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-include <stdio.h>
+#include <stdio.h>
 
 int _sqrt(int n, int i);
 
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-include <stdio.h>
+#include <stdio.h>
 
 int check_prime(int n, int i);
 
